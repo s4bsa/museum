26e436574868f5ad333649d3fc59ee1472398817
s4bsa <sabrina.hung@sjsu.edu>
2025-05-15 00:03:20 -0700

    Add files via upload

---
+++ Enfants/sketch.js
@@ -0,0 +1,14 @@
+let topImg;
+
+function preload() {
+  topImg = loadImage('https://s4bsa.github.io/diyps/enfants1.jpg');
+}
+
+function setup() {
+  createCanvas(600, 600);
+  cursor('https://s4bsa.github.io/diyps/glass.png', -20, 10); 
+  image(topImg, 0, 0);
+}
+
+function draw() {
+}
